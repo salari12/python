8e9b4deeb2076ae04082aaa2717f04d153f98527
sobhan <99309178+salari12@users.noreply.github.com>
2023-01-31 21:13:38 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+i=int(input("enter your number ..."))
+even_list=[]
+odd_list=[]
+for i in range(i):
+    if i %2==0 :
+        even_list.append(i)
+    elif i %2 !=0:
+        odd_list.append(i)
+print(odd_list)
+print(even_list)
+
